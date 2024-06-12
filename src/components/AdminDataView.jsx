783405175d3cd6ff544b5838aac3c56d1d92650e
Meet Jasani <meetjjasani@Meets-MacBook-Pro.local>
2024-06-12 21:32:07 +0530

    admin-dashboard

--- src/components/AdminDataView.jsx
+++ src/components/AdminDataView.jsx
@@ -9,7 +9,7 @@ const   AdminDataView = ({data , index , setOpen}) => {
             <div className="colHeader">{data.paperTitle}</div>
             <div className="colHeader">{data.createdAt.split('T')[0]}</div>
             <div className="colHeader"><span className={data.status.status}>{data.status.status}</span></div>
-            <div className="colHeader">Admin | <FaRegEye onClick={setOpen} /> | <MdDeleteOutline /></div>
+            <button className="colHeader" style={{cursor:'pointer'}} onClick={setOpen} >View</button>
         </>
     )
 }
--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,22 +1,51 @@
-import React, { useEffect, useRef } from 'react';
-import { AiOutlineCloseCircle } from 'react-icons/ai';
-import useOnClickOutside from '../customHooks/useOnClickOutside';
+import React, { useEffect, useRef, useState } from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import useOnClickOutside from "../customHooks/useOnClickOutside";
+import { set } from "mongoose";
 
 const PopUp = ({ setOpen }) => {
   const popUpRef = useRef(null);
 
   // Use the useOnClickOutside hook to handle clicks outside the popup
-  useOnClickOutside(popUpRef,  () => setOpen(false));
+  // useOnClickOutside(popUpRef,  () => setOpen(false));
+  const [reasonfield, setReasonfield] = useState(false);
+  const [okbutton, setokbutton] = useState(false);
+  const showField = () => setReasonfield(true);
+  const hideField = () => setReasonfield(false);
 
   return (
-    <div className="absoulutContainer">
-      <div ref={popUpRef} className="approvalContainer">
-        <div className="approvalTitle">
-          Take Action
-          <AiOutlineCloseCircle className="iconBigger" onClick={()=>setOpen(false)} />
+    <>
+      <div className="absoulutContainer">
+        <div ref={popUpRef} className="approvalContainer">
+          <div className="approvalTitle">
+            Take Action
+            <AiOutlineCloseCircle
+              className="iconBigger"
+              onClick={() => setOpen(false)}
+            />
+          </div>
+          <div className="admin-main-container">
+            <button onClick={hideField} className="admin-approve">
+              Approve
+            </button>
+            <button onClick={showField} className="admin-reject">
+              Reject
+            </button>
+            <button onClick={showField} className="admin-return">
+              Return
+            </button>
+          </div>
+          {reasonfield && (
+            <div className="admin-reason">
+              <textarea placeholder="Reason" type="text" />
+            </div>
+          )}
+          <div className="admin-ok-status">
+            <button>OK</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
